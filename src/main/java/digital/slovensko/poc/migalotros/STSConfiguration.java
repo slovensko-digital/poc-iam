b893e099cc7ea5232011f2e1eebd2d370d36b33f
Jan Suchal <jan.suchal@gmail.com>
2026-02-03 11:03:20 +0100

    Add custom SAML assertions handler and improve STS token handling

--- src/main/java/digital/slovensko/poc/migalotros/STSConfiguration.java
+++ src/main/java/digital/slovensko/poc/migalotros/STSConfiguration.java
@@ -6,9 +6,12 @@ import org.apache.cxf.sts.StaticSTSProperties;
 import org.apache.cxf.sts.operation.TokenIssueOperation;
 import org.apache.cxf.sts.service.StaticService;
 import org.apache.cxf.sts.token.provider.SAMLTokenProvider;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
 import org.apache.cxf.ws.security.sts.provider.SecurityTokenServiceProvider;
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,7 +31,7 @@ public class STSConfiguration {
         var props = new StaticSTSProperties();
         props.setSignatureCryptoProperties("stsstore.properties");
         props.setSignatureUsername("stskey");
-        props.setIssuer("DigitalSlovenskoSTS");
+        props.setIssuer("iamsts.upvsfix.local");
         return props;
     }
 
@@ -42,7 +45,12 @@ public class STSConfiguration {
     @Bean
     public TokenIssueOperation issueDelegate(StaticService eksService, StaticSTSProperties stsProperties) {
         var issue = new TokenIssueOperation();
-        issue.setTokenProviders(List.of(new SAMLTokenProvider()));
+        
+        // Create SAML token provider with custom handler
+        var samlTokenProvider = new SAMLTokenProvider();
+        samlTokenProvider.setSamlCustomHandler(new AddUPVSSamlAssertionsHandler());
+        
+        issue.setTokenProviders(List.of(samlTokenProvider));
         issue.setServices(List.of(eksService));
         issue.setStsProperties(stsProperties);
         return issue;
@@ -59,14 +67,12 @@ public class STSConfiguration {
     public WSS4JInInterceptor stsWss4jInInterceptor() {
         var inProps = new HashMap<String, Object>();
 
-        // Skip signature verification by not processing the Signature action
-         inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.SIGNATURE + " " + ConfigurationConstants.TIMESTAMP);
-//        inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.TIMESTAMP);
+        inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.SIGNATURE + " " + ConfigurationConstants.TIMESTAMP);
 
         inProps.put(ConfigurationConstants.SIG_PROP_FILE, "stsstore.properties");
         inProps.put(ConfigurationConstants.IS_BSP_COMPLIANT, "false");
 
-        // DEBUG onlu - disable strict Timestamp expiry handling and relax range checks
+        // DEBUG only - disable strict Timestamp expiry handling and relax range checks
         inProps.put(ConfigurationConstants.TIMESTAMP_STRICT, "false");
         inProps.put(ConfigurationConstants.TTL_TIMESTAMP, "144000000"); // allow up to 4h old
         inProps.put(ConfigurationConstants.TTL_FUTURE_TIMESTAMP, "600000000"); // allow 10 min in the future
@@ -77,19 +83,23 @@ public class STSConfiguration {
         return new WSS4JInInterceptor(inProps);
     }
 
-    /**
-     * Publish the STS endpoint at /STS, SOAP 1.2 HTTP binding.
-     * <p>
-     * Equivalent to XML configuration:
-     * <jaxws:endpoint id="stsEndpoint" implementor="#stsProviderBean" address="/STS" bindingUri="http://www.w3.org/2003/05/soap/bindings/HTTP/">
-     * <jaxws:inInterceptors>...</jaxws:inInterceptors>
-     * </jaxws:endpoint>
-     */
     @Bean
-    public jakarta.xml.ws.Endpoint stsEndpoint(SecurityTokenServiceProvider stsProviderBean, WSS4JInInterceptor stsWss4jInInterceptor) {
+    public WSS4JOutInterceptor stsWss4jOutInterceptor() {
+        var props = new HashMap<String, Object>();
+
+        props.put(WSHandlerConstants.ACTION, WSHandlerConstants.TIMESTAMP);
+        props.put(WSHandlerConstants.TTL_TIMESTAMP, "3600");
+
+        return new WSS4JOutInterceptor(props);
+    }
+
+    @Bean
+    public jakarta.xml.ws.Endpoint stsEndpoint(SecurityTokenServiceProvider stsProviderBean, WSS4JInInterceptor stsWss4jInInterceptor, WSS4JOutInterceptor stsWss4jOutInterceptor) {
         var endpoint = new EndpointImpl(bus, stsProviderBean);
         endpoint.setBindingUri("http://www.w3.org/2003/05/soap/bindings/HTTP/");
+        endpoint.getFeatures().add(new WSAddressingFeature());
         endpoint.setInInterceptors(Collections.singletonList(stsWss4jInInterceptor));
+        endpoint.setOutInterceptors(Collections.singletonList(stsWss4jOutInterceptor));
         endpoint.publish("/STS");
         return endpoint;
     }
